b2dd8f571e3953d91c4c8020747f98a202fe3e69
pschake34 <pschake34@charlottesvilleschools.org>
2023-02-06 10:30:00 -0500

    Add 2 non-functional prototypes

---
+++ code/prototypes/full_speed.py
@@ -0,0 +1,17 @@
+from adafruit_motor import motor
+import board
+import pwmio
+import time
+
+A1 = board.GP10
+A2 = board.GP11
+pwmA1 = pwmio.PWMOut(A1, frequency=50)
+pwmA2 = pwmio.PWMOut(A2, frequency=50)
+motor1 = motor.DCMotor(pwmA1, pwmA2)
+motor1.SLOW_DECAY
+
+while True:
+    motor1.throttle = 0.5
+    time.sleep(5)
+    motor1.throttle = 0
+    time.sleep(2)
