2bb515f294df0ccbcff75f381e14840e3dd6410e
pschake34 <pschake34@charlottesvilleschools.org>
2023-03-08 10:40:38 -0500

    Add new accelerometer to PID

--- code/prototypes/PID.py
+++ code/prototypes/PID.py
@@ -3,7 +3,7 @@
 
 import board
 import pwmio
-import adafruit_mpu6050
+from adafruit_lsm6ds.lsm6dso32 import LSM6DSO32
 import busio
 import time
 import math
@@ -17,17 +17,24 @@ motor2_pwm = pwmio.PWMOut(MOTOR_2, frequency=1000)
 motor3_pwm = pwmio.PWMOut(MOTOR_3, frequency=1000)
 motor4_pwm = pwmio.PWMOut(MOTOR_4, frequency=1000)
 
-sda_pin = board.GP14
-scl_pin = board.GP15
+sda_pin = board.GP16
+scl_pin = board.GP17
 i2c = busio.I2C(scl_pin, sda_pin)
 
-mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68)
+sensor = LSM6DSO32(i2c)
 
 last_update = time.monotonic()
 
 Kp = 0.5
 Ki = 0.01
 Kd = 0.01
+
+motor1_baseline = 0.7
+motor2_baseline = 0.7
+motor3_baseline = 0.7
+motor4_baseline = 0.7
+
+PID_multiplier = 100
 
 error_pitch = 0
 error_roll = 0
@@ -56,8 +63,8 @@ roll_PID = 0
 yaw_PID = 0
 
 while True:
-    x = mpu.acceleration[0]
-    y = mpu.acceleration[1]
+    x = sensor.acceleration[0]
+    y = sensor.acceleration[1]
     if x == 0:
         x = 0.000001
     angle = math.degrees(math.atan(y/x))
@@ -73,39 +80,11 @@ while True:
 
     now = time.monotonic()
     dt = now - last_update
-    
-    ## Find errors for pitch
-    #if angle > 0 and angle < 180:
-    #    error_pitch = -1 * (setpoint - intensity)
-    #elif angle > 180 and angle < 360:
-    #    error_pitch = setpoint - intensity
-    #else:
-    #    error_pitch = 0
 
-    ## Find errors for roll
-    #if angle > 105 and angle < 165:
-    #    error_roll = -1 * (setpoint - intensity)
-    #elif angle > 285 and angle < 345:
-    #    error_roll = setpoint - intensity
-    #else:
-    #    error_roll = 0
+    angle += 45
 
-    ## Find errors for pitch and roll at the same time for specific angles
-    #if angle >= 345 or angle <= 15:
-    #    error_pitch = -1 * (setpoint - intensity)
-    #    error_roll = setpoint - intensity
-    #if angle >= 75 and angle <= 105:
-    #    error_pitch = -1 * (setpoint - intensity)
-    #    error_roll = error_pitch
-    #if angle >= 165 and angle <= 195:
-    #    error_pitch = setpoint - intensity
-    #    error_roll = -1 * (setpoint - intensity)
-    #if angle >= 255 and angle <= 285:
-    #    error_pitch = setpoint - intensity
-    #    error_roll = setpoint - intensity
-
-    error_pitch = setpoint - (intensity * math.cos(angle))
-    error_roll = setpoint - (intensity * math.sin(angle))
+    error_pitch = setpoint - (intensity * math.sin(math.radians(angle)))
+    error_roll = setpoint - (intensity * math.cos(math.radians(angle)))
 
     integral_pitch = integral_pitch + dt * error_pitch
     integral_roll = integral_roll + dt * error_roll
@@ -118,11 +97,10 @@ while True:
 
     
 
-    motor1_pwm.duty_cycle = 65535 // 4  # Cycles the pin with 50% duty cycle (half of 2 ** 16)
-    motor2_pwm.duty_cycle = 65535 // 4  # Cycles the pin with 50% duty cycle (half of 2 ** 16)
-    motor3_pwm.duty_cycle = 65535 // 4  # Cycles the pin with 50% duty cycle (half of 2 ** 16)
-    motor4_pwm.duty_cycle = 65535 // 4  # Cycles the pin with 50% duty cycle (half of 2 ** 16)
-
+    motor1_pwm.duty_cycle = int(65535 * motor1_baseline - pitch_PID * PID_multiplier)
+    motor2_pwm.duty_cycle = int(65535 * motor2_baseline) #int(65535 * motor2_baseline + roll_PID * PID_multiplier)
+    motor3_pwm.duty_cycle = int(65535 * motor3_baseline + pitch_PID * PID_multiplier)
+    motor4_pwm.duty_cycle = int(65535 * motor4_baseline)   #int(65535 * motor4_baseline - roll_PID * PID_multiplier)
     last_error_pitch = error_pitch
     last_error_roll = error_roll
     last_update = now
@@ -130,6 +108,12 @@ while True:
     time.sleep(0.07)
     print("angle: " + str(angle))
     print("initial_intensity: " + str(intensity))
-    print("error_pitch: " + str(error_pitch))
-    print("error_roll: " + str(error_roll))
-    #print(f"x: {round(mpu.acceleration[0], 3)} y: {round(mpu.acceleration[1], 3)} z: {round(mpu.acceleration[2], 3)}")
+    print("motor1_pwm.duty_cycle: " + str(motor1_pwm.duty_cycle))
+    print("motor2_pwm.duty_cycle: " + str(motor2_pwm.duty_cycle))
+    #temp_baseline = float(input("Enter baseline: "))
+    #motor1_baseline = temp_baseline
+    #motor2_baseline = temp_baseline
+    #motor3_baseline = temp_baseline
+    #motor4_baseline = temp_baseline
+
+    #print(f"x: {round(sensor.acceleration[0], 3)} y: {round(sensor.acceleration[1], 3)} z: {round(sensor.acceleration[2], 3)}")
