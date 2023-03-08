9845b51bc41a9474805260eeeb8d392aae430413
pschake34 <pschake34@charlottesvilleschools.org>
2023-03-08 10:08:04 -0500

    Get better accelerometer working

--- code/prototypes/accelerometer_2.py
+++ code/prototypes/accelerometer_2.py
@@ -1,16 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (c) 2020 Bryan Siepert for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
 import time
 import board
+from adafruit_lsm6ds.lsm6dso32 import LSM6DSO32
 import busio
-from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 
+# initialize i2c
 sda_pin = board.GP16
 scl_pin = board.GP17
 i2c = busio.I2C(scl_pin, sda_pin)
 
-sensor = LSM6DSOX(i2c)
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+sensor = LSM6DSO32(i2c)
 
 while True:
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+    print("Acceleration: X:%.4f, Y: %.4f, Z: %.4f m/s^2" % (sensor.acceleration))
+    #print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
     print("")
-    time.sleep(0.5)
+    time.sleep(0.07)
