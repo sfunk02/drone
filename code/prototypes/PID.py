a53ac6267498004f4eb97a0d4c00ac9c851b32c3
pschake34 <pschake34@charlottesvilleschools.org>
2023-03-10 10:46:07 -0500

    Continue working on PID tuning

--- code/prototypes/PID.py
+++ code/prototypes/PID.py
@@ -25,16 +25,16 @@ sensor = LSM6DSO32(i2c)
 
 last_update = time.monotonic()
 
-Kp = 0.5
-Ki = 0.01
+Kp = 0.4
+Ki = 0.1
 Kd = 0.01
 
-motor1_baseline = 0.7
-motor2_baseline = 0.7
-motor3_baseline = 0.7
-motor4_baseline = 0.7
+motor1_baseline = 0.85
+motor2_baseline = 0.8
+motor3_baseline = 0.8
+motor4_baseline = 0.8
 
-PID_multiplier = 100
+PID_multiplier = 700
 
 error_pitch = 0
 error_roll = 0
@@ -72,16 +72,19 @@ while True:
 
     # Find quadrant and make angle between 0 and 360
     if x < 0 and y > 0: # Q2
-        angle += 180
-    elif x < 0 and y < 0: # Q3
+        angle += 360
+    elif x > 0 and y > 0: # Q3
         angle += 180
     elif x > 0 and y < 0: #Q4
-        angle += 360
+        angle += 180
 
     now = time.monotonic()
     dt = now - last_update
 
     angle += 45
+
+    if angle > 360:
+        angle -= 360
 
     error_pitch = setpoint - (intensity * math.sin(math.radians(angle)))
     error_roll = setpoint - (intensity * math.cos(math.radians(angle)))
@@ -95,11 +98,13 @@ while True:
     pitch_PID =  Kp * error_pitch + Ki * integral_pitch + Kd * derivative_pitch
     roll_PID = Kp * error_roll + Ki * integral_roll + Kd * derivative_roll
 
-    
+    PID_scaler = PID_multiplier * intensity
 
-    motor1_pwm.duty_cycle = int(65535 * motor1_baseline - pitch_PID * PID_multiplier)
+    # make sure duty_Cycle never goes above 65535
+
+    motor1_pwm.duty_cycle = int(65535 * motor1_baseline - pitch_PID * PID_scaler)
     motor2_pwm.duty_cycle = int(65535 * motor2_baseline) #int(65535 * motor2_baseline + roll_PID * PID_multiplier)
-    motor3_pwm.duty_cycle = int(65535 * motor3_baseline + pitch_PID * PID_multiplier)
+    motor3_pwm.duty_cycle = int(65535 * motor3_baseline + pitch_PID * PID_scaler)
     motor4_pwm.duty_cycle = int(65535 * motor4_baseline)   #int(65535 * motor4_baseline - roll_PID * PID_multiplier)
     last_error_pitch = error_pitch
     last_error_roll = error_roll
@@ -107,9 +112,9 @@ while True:
 
     time.sleep(0.07)
     print("angle: " + str(angle))
-    print("initial_intensity: " + str(intensity))
+    print("PID_scaler: " + str(PID_scaler))
     print("motor1_pwm.duty_cycle: " + str(motor1_pwm.duty_cycle))
-    print("motor2_pwm.duty_cycle: " + str(motor2_pwm.duty_cycle))
+    print("motor3_pwm.duty_cycle: " + str(motor3_pwm.duty_cycle))
     #temp_baseline = float(input("Enter baseline: "))
     #motor1_baseline = temp_baseline
     #motor2_baseline = temp_baseline
