10c7274bb6d6f5aca3ae3ec4ad9c8558bd4851fa
pschake34 <pschake34@charlottesvilleschools.org>
2023-05-25 10:15:04 -0400

    Add comments and finalize code

--- code/final_code_4.0.py
+++ code/final_code_4.0.py
@@ -9,6 +9,7 @@ import busio
 import time
 import math
 
+# Initialize Motors
 MOTOR_1 = board.GP11
 MOTOR_2 = board.GP21
 MOTOR_3 = board.GP10
@@ -18,23 +19,31 @@ motor2_pwm = pwmio.PWMOut(MOTOR_2, frequency=1000)
 motor3_pwm = pwmio.PWMOut(MOTOR_3, frequency=1000)
 motor4_pwm = pwmio.PWMOut(MOTOR_4, frequency=1000)
 
+# Initialize accelerometer
 sda_pin = board.GP16
 scl_pin = board.GP17
 i2c = busio.I2C(scl_pin, sda_pin)
-
 sensor = LSM6DSO32(i2c)
 
+# Initialize LED
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+# Variables for time tracking
 last_update = time.monotonic()
 start_time = time.monotonic()
 
+# PID values
 Kp = 0.23
-Ki = 0.1 #decrease Ki
+Ki = 0.1
 Kd = 0.0
-
-motor_baseline = 0
 
 PID_multiplier = 1000
 
+# Used for changing speed of all motors
+motor_baseline = 0
+
+# Variables keeping track of data
 error_pitch = 0
 error_roll = 0
 error_yaw = 0
@@ -59,10 +68,6 @@ derivative_pitch = 0
 derivative_roll = 0
 derivative_yaw = 0
 
-throttle = 0
-
-pitch_force = 0
-
 setpoint = 0
 
 pitch_PID = 0
@@ -75,13 +80,13 @@ roll_data = []
 while True:
     x = sensor.acceleration[0]
     y = sensor.acceleration[1]
-    if x == 0:
+    if x == 0: # Prevent div by 0 error
         x = 0.000001
 
     angle = math.degrees(math.atan(y/x))
     intensity = math.sqrt((x**2) + (y**2))
 
-    if intensity > 9.8:
+    if intensity > 9.8: # Make sure we dont have extra intensity
         intensity = 9.8
 
     # Find quadrant and make angle between 0 and 360
@@ -95,13 +100,14 @@ while True:
     now = time.monotonic()
     dt = now - last_update
 
-    angle -= 45
+    angle -= 45 # Change angle to reflect orientation of accelerometer
 
     if angle > 360:
         angle -= 360
 
     run_time = now - start_time
 
+    # Ramp the motors up and down for a short time
     if run_time < 2:
         if motor_baseline < 0.7:
             motor_baseline += 0.005
@@ -111,30 +117,35 @@ while True:
     if run_time > 4:
         break
         
+    
+    error_pitch = setpoint - (intensity * math.cos(math.radians(angle))) # Find error value for pitch
+    last_errors_pitch.append(error_pitch) # Used to average error values
+    avg_error_pitch = sum(last_errors_pitch) / len(last_errors_pitch) # Take average
+    error_roll = setpoint - (intensity * math.sin(math.radians(angle))) # Find error value for roll
+    last_errors_roll.append(error_roll) # Used to average error values
+    avg_error_roll = sum(last_errors_roll) / len(last_errors_roll) # Take average
 
-    error_pitch = setpoint - (intensity * math.cos(math.radians(angle)))
-    last_errors_pitch.append(error_pitch)
-    avg_error_pitch = sum(last_errors_pitch) / len(last_errors_pitch)
-    error_roll = setpoint - (intensity * math.sin(math.radians(angle)))
-    last_errors_roll.append(error_roll)
-    avg_error_roll = sum(last_errors_roll) / len(last_errors_roll)
-
+    # Find integral value for PID
     integral_pitch = integral_pitch + dt * avg_error_pitch
     integral_roll = integral_roll + dt * avg_error_roll
 
+    # Find derivative value for PID
     derivative_pitch = (avg_error_pitch - last_avg_error_pitch)/dt
     derivative_roll = (avg_error_roll - last_avg_error_roll)/dt
 
+    # Find final PID values
     pitch_PID =  Kp * avg_error_pitch + Ki * integral_pitch + Kd * derivative_pitch
     roll_PID = Kp * avg_error_roll + Ki * integral_roll + Kd * derivative_roll
 
-    PID_scaler = PID_multiplier * intensity
+    PID_scaler = PID_multiplier * intensity # Scale PID so it can control the motors better
 
+    # Determining the motor speeds
     motor1_duty_cycle = int(65535 * motor_baseline - pitch_PID * PID_scaler)
     motor2_duty_cycle = int(65535 * motor_baseline - roll_PID * PID_scaler)
     motor3_duty_cycle = int(65535 * motor_baseline + pitch_PID * PID_scaler)
     motor4_duty_cycle = int(65535 * motor_baseline + roll_PID * PID_scaler)
 
+    # Make sure motor speeds don't exceed limits
     if motor1_duty_cycle > 65535:
         motor1_duty_cycle = 65535
     if motor2_duty_cycle > 65535:
@@ -157,50 +168,45 @@ while True:
     print("motor2_pwm.duty_cycle: " + str(motor2_duty_cycle))
     print("motor4_pwm.duty_cycle: " + str(motor4_duty_cycle))
     print("motor_baseline: " + str(motor_baseline))
-    #print("roll_PID: " + str(roll_PID))
     print("pitch_PID: " + str(pitch_PID))
 
-    motor1_pwm.duty_cycle = motor1_duty_cycle   #setting motor speeds
+    # Setting the motor speeds
+    motor1_pwm.duty_cycle = motor1_duty_cycle
     motor2_pwm.duty_cycle = motor2_duty_cycle
     motor3_pwm.duty_cycle = motor3_duty_cycle
     motor4_pwm.duty_cycle = motor4_duty_cycle
+
+    # Update timer
     last_update = now
+
+    # Save last error values for derivative calculation
     last_avg_error_pitch = avg_error_pitch
     last_avg_error_roll = avg_error_roll
 
+    # Update error averaging values
     if len(last_errors_pitch) > 5:
         last_errors_pitch.pop(0)
     if len(last_errors_roll) > 5:
         last_errors_roll.pop(0)
 
+    # Save data for saving to filesystem when flight is done
     pitch_data.append(error_pitch)
     roll_data.append(error_roll)
 
-        
+# Turn motors off when the flight is done
+motor1_pwm.duty_cycle = 0
+motor2_pwm.duty_cycle = 0
+motor3_pwm.duty_cycle = 0
+motor4_pwm.duty_cycle = 0
 
+led.value = True # Turn LED on while saving data
 
 with open("/error.txt", "a") as datalog:
-    motor1_pwm.duty_cycle = 0   #setting motor speeds
-    motor2_pwm.duty_cycle = 0
-    motor3_pwm.duty_cycle = 0
-    motor4_pwm.duty_cycle = 0
-    datalog.write('Pitch_Error,Roll_Error\n')  #title for the log file
+    datalog.write('Pitch_Error,Roll_Error\n')  # Title for the log file
     datalog.flush()
-    for i in range(len(pitch_data)):
-        datalog.write('{},{}\n'.format(pitch_data[i], roll_data[i]))  #storing error data on the log file
+    for i in range(0, len(pitch_data), 10):
+        datalog.write('{},{}\n'.format(pitch_data[i], roll_data[i]))  # Storing error data on the log file
         datalog.flush()
 
-        #time.sleep(.08)
-        #print("\nangle: " + str(angle))
-        #print("pitch_PID: " + str(pitch_PID))
-        #print("integral_pitch: " + str(integral_pitch))
-        #print("derivative_pitch: " + str(derivative_pitch))
-        #print("avg_error_pitch: " + str(avg_error_pitch))
-
-        #temp_baseline = float(input("Enter baseline: "))
-        #motor1_baseline = temp_baseline
-        #motor2_baseline = temp_baseline
-        #motor3_baseline = temp_baseline
-        #motor4_baseline = temp_baseline
-
-        #print(f"x: {round(sensor.acceleration[0], 3)} y: {round(sensor.acceleration[1], 3)} z: {round(sensor.acceleration[2], 3)}")
+led.value = False # Turn LED off
+time.sleep(2)
